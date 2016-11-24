73076c6fd0fa6a7b0c0e7f0d4a3ac3595230fb8d
Edafe Onerhime <edafe.onerhime@opendataservices.coop>
2016-11-24 19:37:12 +0000

    Major modifications to process more files
    Major mods include:
    
    Now uses settings in dicts and lists to process files.
    Now uses a loop to process files
    No longer creates placeholder files
    
    Still no error handling :-/

--- etl_hsds_ohana.py
+++ etl_hsds_ohana.py
@@ -47,113 +47,112 @@ def main():
         print(strftime('%X'), 'HSDS folder is empty')
         return       
 
-    if not os.path.exists(os.path.join(args.hsds,'ohana')):
-        os.makedirs(os.path.join(args.hsds,'ohana'))
+    outputFolder='ohana'
+    if not os.path.exists(os.path.join(args.hsds,outputFolder)):
+        os.makedirs(os.path.join(args.hsds,outputFolder))
 
-####################
-### Convert Orgs ###
-#################### 
-    print(strftime('%X'), 'Records Loaded')
-    fileName = "organizations.csv"
-    print(strftime('%X'), 'Processing Organization Records')
-    if fileName in fileList:
-        df = pd.read_csv(os.path.join(args.hsds,fileName))
-        
-        # Rename url->website, year_incorporated->date_incorporated
-        df.rename(columns={'url':'website', 'year_incorporated':'date_incorporated'}, inplace=True)
-        
-        # Add legal_status, accreditations, funding_sources,licenses
-        df["legal_status"] = ""
-        df["accreditations"] = ""
-        df["funding_sources"] = ""
-        df["licenses"] = ""
-        
-        # Rearrange columns
-        df = df[["id", "name", "alternate_name", "description", "email", "website", "legal_status", "tax_status", "tax_id", "date_incorporated", "accreditations", "funding_sources", "licenses"]]
-        df.to_csv(os.path.join(args.hsds,'ohana', fileName),index=False,na_rep="")          
-        print(strftime('%X'), 'Organization Records:  ', "{:,}".format(df.shape[0]))
+#####################
+### Convert Files ###
+#####################
+    fileNameList = ['Organization','Program','Location','Service','Contact','Phone','Physical_Address']    
+    requiredFiles = ['Organization','Location','Service','Phone']
+    outputFiles = {'Organization':'organizations.csv','Program':'programs.csv','Location':'locations.csv','Service':'services.csv','Contact':'contacts.csv','Phone':'phones.csv','Physical_Address':'addresses.csv'}
+    renameColumns = {'Organization':{'url':'website', 'year_incorporated':'date_incorporated'},
+                     'Service':{'url':'website'}}
+    removeColumns = {'Service':['organization_id'],'Phone':['type'],'Physical_Address':['attention','address_3','address_4']}
+    addtlColumns = {'Organization':['legal_status','accreditations','funding_sources','licenses'], 
+                    'Location':['accessibility','admin_emails','alternate_name','description','email','languages','short_desc','website','virtual'],
+                    'Service':['accepted_payments','description','eligibility','fees','funding_sources','interpretation_services','keywords','languages','required_documents','service_areas','taxonomy_ids'],
+                    'Contact':['location_id'],'Phone':['vanity_number','number_type','country_prefix']}
+    fixColumns = {'Organization':{'website':'cleanURL'},'Location':{'description':'setLocDesc','virtual':'setFalse'},'Service':{'website':'cleanURL','description':'setNoneProvided'},'Phone':{'number_type':'setVoice'}}
+    reArrangeColumns = {'Organization':['id', 'name', 'alternate_name', 'description', 'email', 'website', 'legal_status', 'tax_status', 'tax_id', 'date_incorporated', 'accreditations', 'funding_sources', 'licenses'], 
+                        'Program':['id','organization_id','name','alternate_name'], 
+                        'Location':['id','organization_id','accessibility','admin_emails','alternate_name','description','email','languages','latitude','longitude','name','short_desc','transportation','website','virtual'],
+                        'Service':['id','location_id','program_id','accepted_payments','alternate_name','application_process','description','eligibility','email','fees','funding_sources','interpretation_services','keywords','languages','name','required_documents','service_areas','status','wait_time','website','taxonomy_ids'],
+                        'Contact':['id','location_id','organization_id','service_id','name','title','email','department'],
+                        'Phone':['id','location_id','contact_id','organization_id','service_id','number','vanity_number','extension','department','number_type','country_prefix'],
+                        'Physical_Address':['id','location_id','address_1','address_2','city','state_province','postal_code','country']}                        
+    
+    # Fix programs
+    cleanURL = lambda x: 'http://' + x if 'http' not in x and len(x.strip()) > 0 else x.strip()
+    setLocDesc = lambda x: 'See listed services'
+    setNoneProvided = lambda x: 'None Provided'
+    setTrue = lambda x: True
+    setFalse = lambda x: False    
+    setVoice = lambda x: 'voice'
+    
+    for fileName in fileNameList:
+        print(strftime('%X'), 'Processing',fileName,'Records')
+        csvFileName = fileName.lower()+'.csv'
 
-    fileName = "services.csv"        
-    print(strftime('%X'), 'Processing Services Records')    
-    if fileName in fileList:
-        df = pd.read_csv(os.path.join(args.hsds,fileName))
+        # Check if file exists
+        if fileName in requiredFiles and csvFileName not in fileList:
+            print(strftime('%X'), csvFileName,'not found and is required')
+            return
         
-        # Remove Organization ID
-        del df["organization_id"]
+        if csvFileName not in fileList:
+            print(strftime('%X'), csvFileName,'not found')
+            break    
+            
+        # Load file        
+        df = pd.read_csv(os.path.join(args.hsds,csvFileName))
+        print(strftime('%X'), 'Loaded:', "{:,}".format(df.shape[0]),'records')  
         
-        # Rename url->website
-        df.rename(columns={'url':'website'}, inplace=True)
+        # Replace nan with empty string
+        df.replace(np.nan,'', regex=True, inplace=True)
         
-        # Add new columns
-        df["accepted_payments"] = ""
-        df["eligibility"] = ""
-        df["email"] = ""
-        df["fees"] = ""
-        df["funding_sources"] = ""
-        df["interpretation_services"] = ""
-        df["keywords"] = ""
-        df["languages"] = ""
-        df["required_documents"] = ""
-        df["service_areas"] = ""
-        df["taxonomy_ids"] = ""
-        
-        # Rearrange columns
-        df = df[["id", "program_id", "location_id", "name", "alternate_name", "description", "website", "email", "status", "application_process", "wait_time", "accepted_payments", "eligibility", "email", "fees", "funding_sources", "interpretation_services", "keywords", "languages", "required_documents", "service_areas", "taxonomy_ids"]]
-        df.to_csv(os.path.join(args.hsds,'ohana',fileName),index=False,na_rep="")          
-        print(strftime('%X'), 'Service Records:  ', "{:,}".format(df.shape[0]))
+        # Rename columns 
+        if fileName in renameColumns:
+            renameCol = renameColumns[fileName]
+            df.rename(columns=renameCol, inplace=True) 
+            for k,v in renameCol.items():
+                print(strftime('%X'), 'Renamed:', k, 'to', v) 
 
-    fileName = "locations.csv"        
-    print(strftime('%X'), 'Processing Locations Records')        
-    if fileName in fileList:
-        df = pd.read_csv(os.path.join(args.hsds,fileName))
-
-        # Add new columns
-        df["accessibility"] = ""
-        df["admin_emails"] = ""
-        df["email"] = ""
-        df["languages"] = ""
-        df["short_desc"] = ""
-        df["website"] = ""
-        df["virtual"] = "FALSE"
-
-        # Rearrange columns
-        df = df[["id", "organization_id", "name", "alternate_name", "transportation", "latitude", "longitude", "description", "accessibility", "admin_emails", "email", "languages", "short_desc", "website", "virtual"]]        
-        df.to_csv(os.path.join(args.hsds,'ohana',fileName),index=False,na_rep="")                      
-        print(strftime('%X'), 'Location Records:  ', "{:,}".format(df.shape[0]))
+        # Remove columns
+        if fileName in removeColumns:
+            delColumns = removeColumns[fileName]
+            for delCol in delColumns:
+                if delCol in df.columns.names:
+                    df.drop(delCol, axis=1, inplace=True)
         
-    fileName = "phones.csv"
-    print(strftime('%X'), 'Processing Phone Records')            
-    if not fileName in fileList and "organizations.csv" in fileList:
-        df = pd.read_csv(os.path.join(args.hsds,"organizations.csv"), usecols=["id"])
-        
-        # Add columns
-        df["location_id"] = ""
-        df["contact_id"] = ""
-        df["organization_id"] = df["id"]
-        df["service_id"] = ""
-        df["number"] = "000-000-0000"
-        df["vanity_number"] = ""
-        df["extension"] = ""
-        df["department"] = ""
-        df["number_type"] = "voice"
-        df["country_prefix"] = ""
-        df.to_csv(os.path.join(args.hsds,'ohana',fileName),index=False,na_rep="")                      
-        print(strftime('%X'), 'Phone Records:  ', "{:,}".format(df.shape[0]))
+        # Add columns (if they don't exist)
+        if fileName in addtlColumns:
+            newColumns = addtlColumns[fileName]
+            for newCol in newColumns:
+                if not newCol in df.columns.names:
+                    df[newCol] = ""
 
-    fileName = "addresses.csv"
-    print(strftime('%X'), 'Processing Address Records')                
-    if not fileName in fileList and "locations.csv" in fileList:
-        df = pd.read_csv(os.path.join(args.hsds,"locations.csv"), usecols=["id"])
-
-        df["location_id"] = df["id"]
-        df["address_1"] = "-Placeholder-"
-        df["address_2"] = "-Placeholder-"
-        df["city"] = "Miami"
-        df["state_province"] = "FL"
-        df["postal_code"] = "33143"
-        df["country"] = "US"
-        df.to_csv(os.path.join(args.hsds,'ohana',fileName),index=False,na_rep="")                      
-        print(strftime('%X'), 'Address Records:  ', "{:,}".format(df.shape[0]))
+        # Fix columns                               
+        if fileName in fixColumns:
+            fixCol = fixColumns[fileName]
+            for k,v in fixCol.items():
+                if v == 'cleanURL':
+                    df[k] = df[k].fillna("").apply(cleanURL)
+                if v == 'setTrue':
+                    df[k] = df[k].fillna("").apply(setTrue)
+                if v == 'setFalse':
+                    df[k] = df[k].fillna("").apply(setFalse)
+                if v == 'setLocDesc':
+                    df[k] = df[k].fillna("").apply(setLocDesc)
+                if v == 'setNoneProvided':
+                    df[k] = df[k].fillna("").apply(setNoneProvided)  
+                if v == 'setVoice':
+                    df[k] = df[k].fillna("").apply(setVoice)                                            
+                print(strftime('%X'), 'Fixed:', k, 'column using', v) 
+                
+        # Rearrange columns                
+        if fileName in reArrangeColumns:
+            reArrangeCol = reArrangeColumns[fileName]
+            df = df[reArrangeCol]
+            print(strftime('%X'), 'Re-arranged columns') 
+            
+        # Output file
+        if fileName not in outputFiles:
+            print(strftime('%X'), 'output file name not found')
+            return            
+        outFileName = outputFiles[fileName]    
+        df.to_csv(os.path.join(args.hsds,outputFolder, outFileName),index=False,na_rep='')          
+        print(strftime('%X'), 'Created output file',os.path.join(args.hsds,outputFolder, outFileName)) 
         
     print(strftime('%X'), 'Complete')    
 if __name__ == "__main__":
